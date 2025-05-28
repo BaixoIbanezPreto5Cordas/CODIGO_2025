56d00276448ca6da691d8af3cd3af552e9fcc688
BX5C <164504362+BaixoIbanezPreto5Cordas@users.noreply.github.com>
2025-05-28 08:39:34 -0300

    Update cod.c

--- cod.c
+++ cod.c
@@ -1,4 +1,6 @@
-#include <stdio.h>
+//códigos dia 23/05/22025
+
+/* #include <stdio.h>
 
 int main() {
   int livro1 = 0, livro2 = 0; 
@@ -14,3 +16,35 @@ int main() {
     printf("O ID de sua compra é: %f", IDdaCompra);
     return 0;
 }
+*/
+
+//códigos dia 28/05/2025
+/*
+  #include <stdio.h>
+int main () {
+  int num = 0;
+  printf("Digite um número de sua escolha\n");
+  scanf("%d", &num);
+  if (num % 2 == 0) {
+      printf("O número que você digitou é par\n");
+  } else {
+      printf("O número é ímpar\n");
+  }
+  return 0;
+}
+*/
+
+  #include <stdio.h>
+int main () {
+  int ano = 0;
+  printf("Digite um ano de sua escolha\n");
+  scanf("%d", &ano);
+  if ((ano % 4 == 0 &&ano %100 != 0) || (ano % 400 == 0)){
+      printf("O ano que você digitou é bissexto");
+  } else {
+      printf("O ano não é bissexto");
+  }
+  return 0;
+}
+
+
